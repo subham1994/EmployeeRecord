3c625090c95864a14c06905234dfffcb4b3ba503
subham <shubham.gaurav.1994@gmail.com>
2017-03-26 12:40:24 +0530

    add Company and Employee tables

--- records/models.py
+++ records/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+	id = models.CharField(max_length=64, primary_key=True)
+	name = models.CharField(max_length=30, blank=False)
+	created_at = models.DateField(blank=False)
+
+	class Meta:
+		verbose_name_plural = "Companies"
+
+	def __repr__(self):
+		return '{name}'.format(name=self.name)
+
+
+class Employee(models.Model):
+	employee_id = models.CharField(max_length=64, unique=True)
+	first_name = models.CharField(max_length=30, blank=False)
+	last_name = models.CharField(max_length=30, blank=False)
+	designation = models.CharField(max_length=100, blank=False)
+	company = models.ForeignKey(Company, related_name="employees")
+	joining_date = models.DateField(blank=False)
+
+	class Meta:
+		verbose_name_plural = "Employees"
+
+	def __repr__(self):
+		return '{fn} {ln}'.format(fn=self.first_name, ln=self.last_name)
